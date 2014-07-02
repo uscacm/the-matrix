9b554ce89b9b2f8c426f5b206a38249bb2133463
jjw1707 <jjw1707@gmail.com>
2014-07-01 23:36:26 -0700

    added saving new user to db functionality.

--- db/user_driver.js
+++ db/user_driver.js
@@ -23,4 +23,18 @@ UserDriver.prototype.findAll = function(callback) {
   });
 };
 
+UserDriver.prototype.save = function(user, callback) {
+  this.getCollection(function(err, user_collection) {
+    if (err) callback(err);
+    else {
+      user.created_at = new Date();
+      // TODO(jjw1707) Generation of hashed link
+
+      user_collection.insert(user, function() {
+        callback(null, user);
+      });
+    }
+  });
+};
+
 exports.UserDriver = UserDriver;
--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,40 @@ router.get('/list_users', function(req, res) {
   });
 });
 
+router.get('/new', function(req, res) {
+  res.render('test_new_user');
+});
+
+router.post('/add', function(req, res) {
+  var user_driver = req.user_driver;
+
+  // Get user information
+  var usc_id = req.body.usc_id;
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+  var grad_year = req.body.grad_year;
+  var grad_student = req.body.grad_student;
+  var email = req.body.email;
+  var major = req.body.major;
+
+  var user = {
+    "usc_id": usc_id,
+    "first_name": first_name,
+    "last_name": last_name,
+    "graduation_year": grad_year,
+    "grad_student": grad_student,
+    "email": email,
+    "major": major
+  };
+
+  user_driver.save(user, function(err, doc) {
+    if (err) {
+      res.send("ERROR");
+    } else {
+      res.location('list_users');
+      res.redirect('list_users');
+    }
+  });
+});
+
 module.exports = router;
