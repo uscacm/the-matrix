05026348a9dd781bb74b338f3799b35205568cd3
colortheskyjade <fading.jade@gmail.com>
2014-11-17 14:57:41 -0800

    manual entry

--- routes/checkin.js
+++ routes/checkin.js
@@ -3,42 +3,46 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/', function(req, res) {
-  res.render('checkin', { title: 'Express' });
+  res.render('checkin', { title: 'Check In' });
 });
 
 // TODO(ruyan)
 router.post('/action', function(req, res) {
   var user_driver = req.user_driver;
+  var userID;
 
-  // If the card scan has an actual entry.
   if (req.body['card-scan'] && 
       req.body['card-scan'].match(/^ *$/) === null && 
-      req.body['card-scan'] !== "") {
+      req.body['card-scan'] !== "") { 
+    // CARD SCANNER ENTRY
+
+    // Capture card scanner data
     var pattern = /w*\+([0-9]{10})w*/;
-    // console.log(req.body['card-scan']);
     var match = pattern.exec(req.body['card-scan']);
+    userID = match[1];
 
     if (match === null) {
       res.sendStatus(500);
-    } else {
-      var err;
-      user_driver.findByUSCID(match[1], function(err, users) {
-        if (err === null) {
-          res.send({'id': users[0]['usc_id'], 'fname': users[0]['first_name'], 'error': 0});
-        } else {
-          //TODO(ruyan): error cases
-          res.send({'error': err.errorcode});
-        }
-      });
     }
-  } else {
-    var log = fs.createWriteStream('message.txt', {'flags': 'a'});
-    // use {'flags': 'a'} to append and {'flags': 'w'} to erase and write a new file
-    log.write(req.body['usc-id']);
-    log.write('\n');
-
-    res.send({'id': req.body['usc-id']});
+  } else { 
+    // MANUAL ENTRY
+    userID = req.body['usc-id'];
   }
+
+  var err;
+  user_driver.findByUSCID(userID, function(err, users) {
+  if (err === null) {
+    res.send({'id': users[0]['usc_id'], 'fname': users[0]['first_name'], 'error': 0});
+  } else {
+    //TODO(ruyan): error cases
+    res.send({'error': err.errorcode});
+  }
+  });
 });
+
+router.get('/paid', function(req, res) {
+  res.render('checkin', { title: 'Check In' });
+});
+
 
 module.exports = router;
